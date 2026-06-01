33ed1e4a7a119e3e560111038414080926627638
Victor Blomqvist <vb@viblo.se>
2026-06-01 19:15:09 +0000

    Swap app icon to MànMàn snail concept

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ export function App() {
       ${focusMode ? null : html`
         <header class="app-header">
           <button class="brand" onClick=${() => go('home')}>
-            <span class="brand-mark"><img src="./assets/icons/icon.svg" alt="" /></span>
+            <span class="brand-mark"><img src="./assets/icons/icon.svg?v=38" alt="" /></span>
             <span><${UiText} progress=${progress} id="app.title" /></span>
           </button>
         </header>
